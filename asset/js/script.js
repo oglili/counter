aae9db527521d3ac8fab5e348c378a826c31e872
oghomwen <osazuwaoghomwen@gmail.com>
2022-01-25 14:52:26 +0100

    files added

---
+++ asset/js/script.js
@@ -0,0 +1,8 @@
+let valore = document.querySelector('#valore');
+let valoreDecrementa = document.querySelector('#decrementa');
+let valoreAumenta = document.querySelector('#aumenta');
+
+
+valoreAumenta.addEventListener('click', ()=>{
+    valore.textContent++;
+});
